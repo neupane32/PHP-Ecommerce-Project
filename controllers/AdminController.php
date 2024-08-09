f0599113138f5ebd46f2913d3b6cfcab6b724080
Aashish Neupane <np05cp4a220004@iic.edu.np>
2024-08-09 14:17:33 +0545

    Admin Controller

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once './models/Admin.php';  // Add this line to include the Admin model
+require_once './controllers/Controller.php'; // If 'Controller.php' is a base class, include it
+
 class AdminController extends Controller {
     private $admin;
 
@@ -16,9 +19,16 @@ class AdminController extends Controller {
             echo 'Unauthorized';
             exit;
         } else {
+            // Fetch username and password from the request
             $username = $_SERVER['PHP_AUTH_USER'];
             $password = $_SERVER['PHP_AUTH_PW'];
-            $admin = $this->admin->authenticate($username, $password);
+
+            // Base64 encode the username and password
+            $encodedUsername = base64_encode($username);
+            $encodedPassword = base64_encode($password);
+
+            // Authenticate the user using the encoded values
+            $admin = $this->admin->authenticate($encodedUsername, $encodedPassword);
 
             if ($admin) {
                 echo json_encode(["message" => "Login successful", "admin" => $admin]);
