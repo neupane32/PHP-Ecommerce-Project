fc874c25e8f002ca6a5292f4ce02a1f7a592085d
Aashish Neupane <np05cp4a220004@iic.edu.np>
2024-08-09 13:08:26 +0545

    router

--- routes/Router.php
+++ routes/Router.php
@@ -2,11 +2,16 @@
 
 class Router {
     public static function route($url) {
+        // Assuming the controller name is passed in $url[0]
         $controllerName = $url[0] . 'Controller';
         $actionName = isset($url[1]) ? $url[1] : 'index';
 
-        require_once '../app/controllers/' . $controllerName . '.php';
+        // Adjusted path based on your folder structure
+        require_once './controllers/' . $controllerName . '.php';
+
+        // Instantiate the controller
         $controller = new $controllerName();
+        // Call the action method
         $controller->{$actionName}();
     }
 }
