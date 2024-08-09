d8108e3511bfa389a0c79f0de901361c1c25e583
Aashish Neupane <np05cp4a220004@iic.edu.np>
2024-08-09 12:45:21 +0545

    index file

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
+<?php
 
-<?Php
 require_once './config/database.php';
-require_once './controllers/AdminController.php';
-
-$db_name = (new Database())->getConnection();
-$adminController = new AdminController($db_name);
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $response = $adminController->authenticate();
-    header('Content-Type: application/json');
-    echo json_encode($response);
-} else {
-    http_response_code(405);
-    echo json_encode(['success' => false, 'message' => 'Invalid request method.']);
-}
+require_once './controllers/controller.php';
+require_once './routes/Router.php';
+require_once './routes/web.php';
