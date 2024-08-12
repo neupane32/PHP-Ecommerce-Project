080f3668c8e08aa66e13e00c5851b298f8d3ec5f
Aashish Neupane <np05cp4a220004@iic.edu.np>
2024-08-12 10:34:14 +0545

    Admin Model

--- controllers/controller.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Controller {
-    // Base Controller class which can be extended by other controllers
-}
--- models/Admin.php
+++ models/Admin.php
@@ -1,31 +1,23 @@
 <?php
-
 class Admin {
     private $conn;
     private $table_name = "admins";
-
-    public $id;
-    public $username;
-    public $password;
-    public $role;
 
     public function __construct($db) {
         $this->conn = $db;
     }
 
     public function authenticate($username, $password) {
-        $query = "SELECT * FROM " . $this->table_name . " WHERE username = ? LIMIT 0,1";
-
+        $query = "SELECT * FROM " . $this->table_name . " WHERE username = ? AND password = ?";
         $stmt = $this->conn->prepare($query);
-        $stmt->bind_param('s', $username);
+        $stmt->bind_param("ss", $username, $password);
         $stmt->execute();
         $result = $stmt->get_result();
-        $admin = $result->fetch_assoc();
-
-        if ($admin && password_verify($password, $admin['password'])) {
-            return $admin;
+        
+        if ($result->num_rows > 0) {
+            return $result->fetch_assoc();
+        } else {
+            return false;
         }
-
-        return false;
     }
 }
